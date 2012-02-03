53a31e324114c93c426ca105a92501bbe0ae6c1f
James Hurford <terrasea@gmail.com>
2012-02-03 18:04:25 +1300

    Modified users.py
    Removed default value for get_user(), you now have to supply a uid.
    This is and effort to remove the dependency on cherrypy.
    
    Also made it so users of uid of None can't be created.
    
    Added more validity checks and outputted more useful log messages,
    using logging.  Made sure that the string concatenation can take place
    even if the var is None, by using the str(var) function.

--- database/user.py
+++ database/user.py
@@ -1,33 +1,39 @@
 from DB import DB, SERVER, PORT
 from models import User
+import transaction
+import logging
+import cherrypy
+from copy import deepcopy
 
-def get_password(username):
+def get_password(uid):
     db = DB(SERVER, PORT)
     try:
-        if username in db.dbroot:
-            user = db.dbroot[username]
+        if uid in db.dbroot:
+            user = db.dbroot[uid]
             return user.password
+        else:
+            logging.warn('get_password: user of uid ' + str(uid) + \
+                         ' does not exist')
     except Exception, e:
-        logging.error(''.join(['get_password(): ', str(e)]))
+        logging.error('get_password(): ' + str(e))
     finally:
         db.close()
 
     return None
 
 
-def get_user(uid=None):
+def get_user(uid):
     db = DB(SERVER, PORT)
     try:
-        if uid is not None:
+        if uid in db.dbroot:
             user = db.dbroot[uid]
 
             return deepcopy(user)
-        elif cherrypy.request.login in db.dbroot:
-            user = db.dbroot[cherrypy.request.login]
-    
-            return deepcopy(user)
+        else:
+            logging.warn('get_user: user of uid ' + str(uid) + \
+                         ' does not exist')
     except Exception, e:
-        logging.error(''.join(['get_user: ', str(e)]))
+        logging.error('get_user: ' + str(e))
     finally:
         db.close()
 
@@ -35,6 +41,10 @@ def get_user(uid=None):
 
 
 def add_user(uid, password, firstname=None, lastname=None):
+    if not uid or not password:
+        logging.error("add_user: can't add a user of uid " + str(uid) + \
+                      " and password of " + str(password))
+        return None
     user = User(uid, password, firstname, lastname)
     db = DB(SERVER, PORT)
     try:
@@ -57,9 +67,36 @@ def add_user(uid, password, firstname=None, lastname=None):
 
 
 
-def delete_user(user):
-    pass
+def delete_user(uid):
+    db = DB(SERVER, PORT)
+    try:
+        if uid in db.dbroot:
+            del db.dbroot[uid]
+            transaction.commit()
+            return True
+        else:
+            logging.warn('delete_user: user of uid ' + str(uid) + \
+                         ' does not exist')
+    except Exception, e:
+        logging.error('Error deleting user with uid ' + str(uid) + \
+                      ': ' + str(e))
+    finally:
+        db.close()
 
+    return False
 
 if __name__ == '__main__':
-    pass
+    add_user('jim', 'password')
+    add_user(None, None)
+    user = get_user(None)
+    user = get_user('jimbo')
+    if user == None:
+        user = get_user('jim')
+    password = get_password('jimbo')
+    if password == None:
+        password = get_password(u'jim')
+    if user and password:
+        print user.id, user.password, password
+    delete_user('jimbo')
+    delete_user('jim')
+    delete_user(None)
